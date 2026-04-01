62cae89231d2c873a975295f8bb83af9e3ef8ec8
WhiteRabbit187 <heallscream@mail.ru>
2026-04-01 12:24:49 +0300

    testcomm

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ramnd-Water Spirit-0.0.1";
+const contentToCache = [
+    "Build/ws_build_0.0.1f.loader.js",
+    "Build/ws_build_0.0.1f.framework.js",
+    "Build/ws_build_0.0.1f.data",
+    "Build/ws_build_0.0.1f.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
